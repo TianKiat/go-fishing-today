c61a3021ffd0f11eb65ce7cbe4aae9b2c3388b2a
Tian Kiat <ngtiankiat97@gmail.com>
2022-10-12 22:05:21 +0800

    Updated Base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fishing-app/",
+  base: "tiankiat.github.io/go-fishing-app",
   plugins: [react()],
 });
